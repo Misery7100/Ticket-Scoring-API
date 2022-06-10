68d1028effde0d19506225186a18b3b84ec2b846
Lev Litvinov <misery7100@gmail.com>
2022-06-10 18:17:49 +0300

    minor refr

---
+++ ticket_scoring_api/api_v1/testing/test_serializers.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from .factories import TicketFactory
+
+# ------------------------------ #
+
+# class TicketSerializer(TestCase):
+
+#     def test_model_fields(self):
+
+#         ticket = TicketFactory()
+#         for field_name in [...]:
+#             self.assertEqual(
+#                 ticket.serializer.data[field_name],
+#                 getattr(ticket, field_name)
+#             )
+
+# # ------------------------------ #
